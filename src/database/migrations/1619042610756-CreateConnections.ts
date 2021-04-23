58681521fbc32714cfb267f59f5fbbfe7f59b037
Bruno Souza <bruno1994deilson@gmail.com>
2021-04-23 08:29:23 -0300

    Erro table sttings DB

--- src/database/migrations/1619042610756-CreateConnections.ts
+++ src/database/migrations/1619042610756-CreateConnections.ts
@@ -1,58 +1,58 @@
-import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
 export class CreateConnections1619042610756 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'connections',
+        name: "connections",
         columns: [
           {
-            name: 'id',
-            type: 'uuid',
+            name: "id",
+            type: "uuid",
             isPrimary: true,
           },
           {
-            name: 'admin_id',
-            type: 'uuid',
+            name: "admin_id",
+            type: "uuid",
             isNullable: true,
           },
           {
-            name: 'user_id',
-            type: 'uuid',
+            name: "user_id",
+            type: "uuid",
           },
           {
-            name: 'socket_id',
-            type: 'varchar',
+            name: "socket_id",
+            type: "varchar",
           },
           {
-            name: 'created_at',
-            type: 'timestamp',
-            default: 'now()',
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
           },
           {
-            name: 'updated_at',
-            type: 'timestamp',
-            default: 'now()',
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
           },
         ],
       }),
     );
 
     await queryRunner.createForeignKey(
-      'connections',
+      "connections",
       new TableForeignKey({
-        name: 'FKConnectionUser',
-        referencedTableName: 'users',
-        referencedColumnNames: ['id'],
-        columnNames: ['user_id'],
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE',
+        name: "FKConnectionUser",
+        referencedTableName: "users",
+        referencedColumnNames: ["id"],
+        columnNames: ["user_id"],
+        onDelete: "SET NULL",
+        onUpdate: "CASCADE",
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('connections', 'FKConnectionUser');
-    await queryRunner.dropTable('connections');
+    await queryRunner.dropForeignKey("connections", "FKConnectionUser");
+    await queryRunner.dropTable("connections");
   }
 }
